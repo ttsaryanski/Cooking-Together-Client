cc18188b00acae194ba61e5336b19055f06267f0
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-12-17 15:57:24 +0200

    Update app.interceptor.ts

--- src/app/interceptors/app.interceptor.ts
+++ src/app/interceptors/app.interceptor.ts
@@ -25,7 +25,7 @@ export const appInterceptor: HttpInterceptorFn = (req, next) => {
     catchError((err) => {
       if (err.status === 401 && err.error?.message === 'Invalid token!') {
         //router.navigate(['/home']);
-      } else if (err.status === 404) {
+      } else if (err.status === 404 && err.error?.message === "Not item whit this id") {
 errorMsgService.setError(err);
 router.navigate(['/404']);
      }else {

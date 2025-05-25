060eeafe694ee7952ea1e2b043f3ad3b320f3596
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-05-25 20:26:23 +0300

    Small fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: "http://localhost:3000/api/coocking",
+    apiUrl: "http://localhost:3000/api/cooking",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: "https://my-rest-api-239117983258.europe-west4.run.app/api/coocking",
+    apiUrl: "https://my-rest-api-239117983258.europe-west4.run.app/api/cooking",
     production: true,
 };
